53f60e6fe25b2599f21e566972654a91739f2125
BomberFish <dalek77isindahouse2@gmail.com>
2021-07-25 15:23:08 -0400

    Actually add the files

---
+++ app/index.js
@@ -0,0 +1,8 @@
+/*
+ * Entry point for the watch app
+ */
+import * as document from "document";
+
+let demotext = document.getElementById("demotext");
+demotext.text = "HAHA U GOT RICKROLLED";
+console.log("Let the rickrolling commence.")
